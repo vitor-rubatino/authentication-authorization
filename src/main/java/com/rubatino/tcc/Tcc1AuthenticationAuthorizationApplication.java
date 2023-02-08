5484a1e7b481b6e1902b36dbeabe127e71def95c
Vitor Rubatino <webstamp.unifal@gmail.com>
2023-02-08 18:15:27 -0300

    project created

---
+++ src/main/java/com/rubatino/tcc/Tcc1AuthenticationAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.rubatino.tcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tcc1AuthenticationAuthorizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tcc1AuthenticationAuthorizationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rubatino/tcc/Tcc1AuthenticationAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rubatino.tcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tcc1AuthenticationAuthorizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
